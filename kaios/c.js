17a3911a0b8c9ea2f5dc18d3fa5f213efaf0045e
joe-x5 <Joesmith1052024@gmail.com>
2026-01-11 19:31:17 +0900

    Edit file kaios/c.js

--- kaios/c.js
+++ kaios/c.js
@@ -1,24 +1,3 @@
-// Function to dynamically load another JavaScript file
-function loadScript(src) {
-    return new Promise((resolve, reject) => {
-        const script = document.createElement('script');
-        script.src = src;
-        script.onload = () => resolve();
-        script.onerror = () => reject(new Error(`Failed to load script: ${src}`));
-        document.head.appendChild(script);
-    });
-}
-
-// Load multiple scripts
-Promise.all([
-    loadScript('https://joe-x5.github.io/kaios/notice.js'),
-    loadScript('https://joe-x5.github.io/kaios/b.js')
-]).then(() => {
-    console.log('All scripts loaded successfully!');
-}).catch(error => {
-    console.error(error);
-});
-
 
 
 
