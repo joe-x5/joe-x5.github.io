a1600c74babe6334bfbba9a5a4b14cc08b658032
joe-x5 <Joesmith1052024@gmail.com>
2026-01-24 12:27:21 +0900

    Edit file kaios/server-link.js

--- kaios/server-link.js
+++ kaios/server-link.js
@@ -1,9 +1,8 @@
-
   // Toggle variable: set to true to enable URL replacement, false to disable
   const enableReplacement = true; // Change to false to disable
 
   if (enableReplacement) {
     document.querySelectorAll('iframe').forEach(function(iframe) {
-      iframe.src = iframe.src.replace('x0storekaios.blogspot.com', 'x0allkaios.blogspot.com');
+      iframe.src = iframe.src.replace('x0storekaios.blogspot.com/', 'new.aaryansah.space/tools/safe-link/link.php?p=');
     });
   }
