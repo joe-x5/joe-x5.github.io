69bf3a51d3dffa9322fc66c8cf4f8dba27d85fd2
joe-x5 <Joesmith1052024@gmail.com>
2026-01-24 11:40:43 +0900

    Edit file kaios/css.js

--- kaios/css.js
+++ kaios/css.js
@@ -1,12 +1,27 @@
+const enableStyles = true; // Set to false to disable
+
 const cssUrls = [
   'https://joe-x5.github.io/kaios/css/a.css',
   'https://joe-x5.github.io/kaios/css/b.css',
   'https://joe-x5.github.io/kaios/css/c.css'
 ];
 
-cssUrls.forEach(function(href) {
-  const link = document.createElement('link');
-  link.rel = 'stylesheet';
-  link.href = href;
-  document.head.appendChild(link);
-});
+const links = [];
+
+if (enableStyles) {
+  // Enable styles
+  cssUrls.forEach(function(href) {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = href;
+    document.head.appendChild(link);
+    links.push(link);
+  });
+} else {
+  // Disable styles: remove existing link elements if any
+  links.forEach(function(link) {
+    if (link.parentNode) {
+      link.parentNode.removeChild(link);
+    }
+  });
+}
