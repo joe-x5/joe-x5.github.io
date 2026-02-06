80a231599d9d7ab468b338a3a49648985b3bb0d4
joe-x5 <Joesmith1052024@gmail.com>
2026-02-06 11:46:11 +0900

    Edit file kaios/a.js

--- kaios/a.js
+++ kaios/a.js
@@ -28,9 +28,7 @@ function loadScriptsForDevice() {
 
     const scriptsToLoad = [
         loadScript('https://joe-x5.github.io/kaios/notice.js'),
-loadScript('https://joe-x5.github.io/kaios/notification.js'),
-loadScript('https://joe-x5.github.io/kaios/js/connection.js'),
-loadScript('https://joe-x5.github.io/kaios/js/ban.js'),
+loadScript('https://joe-x5.github.io/kaios/js/browser.js'),
         loadScript('https://joe-x5.github.io/kaios/kai-ads.js'),
 loadScript('https://joe-x5.github.io/kaios/server-link.js'),
 loadScript('https://joe-x5.github.io/kaios/css.js'),
