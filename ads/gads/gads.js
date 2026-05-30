2de2bbdce1a5ad65f52875df33f1c6a40dafe678
joe-x5 <Joesmith1052024@gmail.com>
2026-05-30 11:25:24 +0900

    Create file gads.js

---
+++ ads/gads/gads.js
@@ -0,0 +1,12 @@
+google_ad_client = &quot;pub-1786122365693674&quot;;
+                google_ad_host = &quot;pub-1556223355139109&quot;; 
+                google_ad_width = 200; 
+                google_ad_height = 250; 
+                google_ad_type = &quot;text_image&quot;; 
+                google_color_border = &quot;FFFFFF&quot;; 
+                google_color_bg = &quot;ffffff&quot;; 
+                google_color_link = &quot;0000ff&quot;; 
+                google_color_text = &quot;000000&quot;; 
+                google_color_url = &quot;008000&quot;; 
+                google_page_url = &quot;https://tool-max.blogspot.com/&quot;; 
+                
