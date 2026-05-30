84d42ad8be70617ddd0fbfba050706601655571d
joe-x5 <Joesmith1052024@gmail.com>
2026-05-30 15:29:44 +0900

    Edit file ads/gads/gads.js

--- ads/gads/gads.js
+++ ads/gads/gads.js
@@ -1,12 +1,12 @@
-google_ad_client = &quot;pub-1786122365693674&quot;;
-                google_ad_host = &quot;pub-1556223355139109&quot;; 
-                google_ad_width = 200; 
-                google_ad_height = 250; 
-                google_ad_type = &quot;text_image&quot;; 
-                google_color_border = &quot;FFFFFF&quot;; 
-                google_color_bg = &quot;ffffff&quot;; 
-                google_color_link = &quot;0000ff&quot;; 
-                google_color_text = &quot;000000&quot;; 
-                google_color_url = &quot;008000&quot;; 
-                google_page_url = &quot;https://tool-max.blogspot.com/&quot;; 
-                
+// script.js
+var google_ad_client = "pub-1786122365693674";
+var google_ad_host = "pub-1556223355139109";
+var google_ad_width = 200;
+var google_ad_height = 250;
+var google_ad_type = "text_image";
+var google_color_border = "FFFFFF";
+var google_color_bg = "ffffff";
+var google_color_link = "0000ff";
+var google_color_text = "000000";
+var google_color_url = "008000";
+var google_page_url = "https://tool-max.blogspot.com/";
