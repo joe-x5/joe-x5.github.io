722f0a07c34cae88d1b9a12eb1498d2c8993d383
joe-x5 <Joesmith1052024@gmail.com>
2026-07-07 18:14:36 +0900

    Create file gads3.js

---
+++ ads/gads/gads3.js
@@ -0,0 +1,12 @@
+// script.js
+var google_ad_client = "pub-1786122365693674";
+var google_ad_host = "pub-6129580795478709";
+var google_ad_width = 200;
+var google_ad_height = 250;
+var google_ad_type = "text_image";
+var google_color_border = "FFFFFF";
+var google_color_bg = "ffffff";
+var google_color_link = "0000ff";
+var google_color_text = "000000";
+var google_color_url = "008000";
+var google_page_url = "https://y8.com/";
