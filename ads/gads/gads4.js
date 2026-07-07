320f4d8dc5f071dd1e70b9c762c054d0266094bb
joe-x5 <Joesmith1052024@gmail.com>
2026-07-07 18:15:17 +0900

    Create file gads4.js

---
+++ ads/gads/gads4.js
@@ -0,0 +1,12 @@
+// script.js
+var google_ad_client = "pub-1786122365693674";
+var google_ad_host = "pub-1556223355139109";
+var google_ad_width = 200;
+var google_ad_height = 250;
+var google_ad_type = "text_image";
+var google_color_border = "FFFFFF";
+var google_color_bg = "ffffff";
+var google_color_link = "0000ff";
+var google_color_text = "000000";
+var google_color_url = "008000";
+var google_page_url = "https://html5portal.com/";
